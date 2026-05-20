a913959426ec4d10183a0b2ea83b466869aa534d
Shivam kumar <shivamjha0224@gmail.com>
2026-05-20 22:44:23 +0530

    fixed responsive

--- himanshu.js
+++ himanshu.js
@@ -34,3 +34,9 @@ window.addEventListener("scroll", () => {
     });
 
 });
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
