344df6e7c53588d3edf0877bed3ff9306acf1af0
Shivam kumar <shivamjha0224@gmail.com>
2026-05-20 21:02:17 +0530

    added portfolio website

---
+++ himanshu.js
@@ -0,0 +1,36 @@
+// SELECT ALL SECTIONS
+
+const sections = document.querySelectorAll("section");
+
+// SELECT NAV LINKS
+
+const navLinks = document.querySelectorAll(".nav-links a");
+
+
+// SCROLL EVENT
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop;
+
+        if(pageYOffset >= sectionTop - 200){
+
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if(link.getAttribute("href") === `#${current}`){
+
+            link.classList.add("active");
+        }
+    });
+
+});
